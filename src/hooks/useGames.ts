c126c1aeed4c5ec3d183a537f7562e0504c3cf2f
Maziar <maziar.malekian@gmail.com>
2024-08-16 10:28:21 +0330

    Implement searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -4,20 +4,25 @@ import { Genre } from "./useGenres";
 import { Platform } from "./usePlatforms";
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform }[];
-    metacritic: number;
-  }
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+}
 
-const useGames = (gameQuery: GameQuery) => 
-    useData<Game>('/games', { 
-      params: { 
-        genres: gameQuery.genre?.id, 
-        platforms: gameQuery.platform?.id, 
+const useGames = (gameQuery: GameQuery) =>
+  useData<Game>(
+    "/games",
+    {
+      params: {
+        genres: gameQuery.genre?.id,
+        platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
-      }}, 
-      [gameQuery]);
+        search: gameQuery.searchText,
+      },
+    },
+    [gameQuery]
+  );
 
 export default useGames;
