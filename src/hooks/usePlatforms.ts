41da84836460cec458aff45b65b39d9bb0fb91bf
Maziar <maziar.malekian@gmail.com>
2024-08-19 11:43:55 +0330

    time calculation with ms component

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
-import APIClient, { FetchResponse } from "../services/api-client";
+import ms from "ms";
 import platforms from "../data/platforms";
+import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Platform>("/platforms");
 
@@ -14,7 +15,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, //24Hr
+    staleTime: ms("24h"),
     initialData: platforms,
   });
 
